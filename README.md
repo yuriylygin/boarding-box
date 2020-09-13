76995d4ac794b08a42790f335ba3d93e2c3fb8fb
Yu.A.Lygin <yuriylygin@gmail.com>
2020-09-13 21:26:46 +0300

    README.md updated. App processes wrong type of a posted image sending a response with 400 status

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-docker container run -it -p 80:80 -v ${pwd}:/src/workspace yuriylygin/boarding-box bash
+# Run Application
 
 docker container run -it -p 80:80  yuriylygin/boarding-box
+
+# Development run
+
+docker container run -it -p 80:80 -v ${pwd}:/src/workspace yuriylygin/boarding-box bash
+
+# Description
+
+Flask wep application 'boarding_box.py' process POST requests with 'file' field which should contain JPEG image.
+
+Response is an array of face locations and five face points for each face.
+
+# Example
+
+curl -F "file=@/mnt/d/py-projects/boarding-box/tests/files/two-persons-3034796.jpg" http://127.0.0.1:80/
+
+Response:
+
+[
+    {
+        "faceLocation": {"topLeftPoint": {"x": 141, "y": 290}, "bottomRightPoint": {"x": 409, "y": 558}}, 
+        "facePoints": [{"x": 350, "y": 358}, {"x": 309, "y": 362}, {"x": 201, "y": 355}, {"x": 246, "y": 361}, {"x": 279, "y": 450}]
+    }, 
+    {
+        "faceLocation": {"topLeftPoint": {"x": 551, "y": 222}, "bottomRightPoint": {"x": 737, "y": 407}}, 
+        "facePoints": [{"x": 700, "y": 270}, {"x": 666, "y": 277}, {"x": 583, "y": 280}, {"x": 616, "y": 281}, {"x": 637, "y": 354}]
+    }
+]
+
+
+# Failed requests
+
+curl -F "file=" http://127.0.0.1:80/
+
+curl -F http://127.0.0.1:80/
--- app/boarding_box.py
+++ app/boarding_box.py
@@ -7,18 +7,20 @@ import tempfile
 app = Flask(__name__)
 api = Api(app)
 
-print(dlib.__version__)
 
 class BoardingBox(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+    parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files', required=True)
 
     def post(self):
         args = self.parser.parse_args()
-        with tempfile.NamedTemporaryFile() as temp:
-            temp.write(args.file.read())
-            image = dlib.load_rgb_image(temp.name)
-            
+
+        if args.file.mimetype in ['image/jpeg']:
+
+            with tempfile.NamedTemporaryFile() as temp:
+                temp.write(args.file.read())
+                image = dlib.load_rgb_image(temp.name)
+                
             face_detector = dlib.get_frontal_face_detector()
             faces = face_detector(image, 1)
             predictor = dlib.shape_predictor('/src/model/shape_predictor_5_face_landmarks.dat')  # эту модель надо скачать https://github.com/davisking/dlib-models/blob/master/shape_predictor_5_face_landmarks.dat.bz2
@@ -27,19 +29,22 @@ class BoardingBox(Resource):
 
             for face in faces:
                 shape = predictor(image, face)
-                face_points = list(map(lambda i:(shape.part(i).x, shape.part(i).y), range(shape.num_parts)))
+                face_points = list(map(lambda i: {'x': shape.part(i).x, 'y': shape.part(i).y}, range(shape.num_parts)))
                 tl_corner = face.tl_corner()
                 br_corner = face.br_corner()
 
                 res.append({
-                    'rectangle': [(tl_corner.x, tl_corner.y), (br_corner.x, br_corner.y)],
+                    'faceLocation': {'topLeftPoint': {'x': tl_corner.x, 'y': tl_corner.y}, 'bottomRightPoint': {'x': br_corner.x, 'y': br_corner.y}},
                     'facePoints': face_points
                 })
 
-        return res
+            return res
+
+        return {'message': {'file': 'JPEG JPG formats are required'}}, 400
+        
         
 
 api.add_resource(BoardingBox, '/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="80", debug=True)
+    app.run(host="0.0.0.0", port="80", debug=False)
